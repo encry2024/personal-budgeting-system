0af2ab1c11f13a85d38136197569b59676656888
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-05-19 10:14:16 +0800

    Created template blade.

---
+++ resources/views/template.blade.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Budgeting System</title>
+
+    <x-header >
+    </x-header>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>

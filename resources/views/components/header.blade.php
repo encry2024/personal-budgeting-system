32c2c94d8ee1057f5f12b572fe331b401c82eda7
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-05-19 23:31:44 +0800

    Added simple CSS class.

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
 <div>
     <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
-    <h1>Test Component Header</h1>
+    <h1 class="text-blue-600">Test Component Header</h1>
 </div>

2edcdeefca3ac3f60114557df60c6ec61f07b40d
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-05-21 07:52:25 +0800

    Updated header blade component to display the authenticated user's first name.

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,6 @@
-<div>
-    <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
-    <h1 class="text-blue-600">Test Component Header</h1>
+<div class="sticky top-0 bg-white pt-3 pb-3 pl-3 mb-3">
+    <h1 class="text-xl">
+        Personal Budgeting System
+        <span class="float-right pr-3 ">Welcome, {{ ucwords(strtolower($userName)) }}</span>
+    </h1>
 </div>

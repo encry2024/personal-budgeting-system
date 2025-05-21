daf0a1e5cbda4846dfc1da6c806758f2051244ae
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-05-21 08:04:27 +0800

    Added dashboard for users.

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('template')
+
+@section('header')
+<x-header userName="{{ $userName }}" />
+@endsection
+
+@section('content')
+<div>
+    <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
+</div>
+@endsection

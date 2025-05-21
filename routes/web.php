d7871a738203492cf1b06731a734965a7737fc29
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-05-21 08:02:04 +0800

    Added route for authenticate, registration page, store user, dashboard, and login page.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
+
+Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
 
 
-Route::get('/', [HomeController::class, 'index']);
+Route::post('authenticate', [LoginController::class, 'authenticate'])->name('authenticate');
+
+Route::get('/user/registration', [UserController::class, 'create'])->name('user.create');
+Route::post('/user/register', [UserController::class, 'store'])->name('user.store');

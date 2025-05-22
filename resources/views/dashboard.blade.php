cafd1e4ca1dce038dd2455d3e63e5d1677a1e7a1
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-05-22 11:08:21 +0800

    Removed header and changed it with Sidebar instead.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,11 @@
 @extends('template')
 
 @section('header')
-<x-header userName="{{ $userName }}" />
+{{-- <x-header userName="{{ $userName }}" /> --}}
 @endsection
 
 @section('content')
+<x-sidebar userName="{{ $userName }}"/>
 <div>
     <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
 </div>

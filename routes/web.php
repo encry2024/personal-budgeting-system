20870dbf6e2a20c0655569484dc68860691c6f04
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-05-25 09:11:10 +0800

    Expense route is placed under Expense route group. Added route group with auth middleware. Added redirect if the user visits / route. Dashboard is placed inside middleware auth route group.

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,23 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ExpenseController;
 
-Route::get('/', [HomeController::class, 'index'])->name('index');
-Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
+Route::get('/', function () {
+    return redirect('/login');
+});
 
+Route::get('/login', [HomeController::class, 'login'])->name('login');
 
 Route::post('authenticate', [LoginController::class, 'authenticate'])->name('authenticate');
 
 Route::get('/user/registration', [UserController::class, 'create'])->name('user.create');
 Route::post('/user/register', [UserController::class, 'store'])->name('user.store');
 
-Route::get('expense', [ExpenseController::class, 'index'])->name('expense.index');
-Route::get('/expense/create', [ExpenseController::class, 'create'])->name('expense.create');
-Route::post('/expense/store', [ExpenseController::class, 'store'])->name('expense.store');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
+
+    Route::group(['prefix' => 'expense', 'name' => 'expense.'], function () {
+        Route::get('expense', [ExpenseController::class, 'index'])->name('index');
+        Route::get('/expense/create', [ExpenseController::class, 'create'])->name('create');
+        Route::post('/expense/store', [ExpenseController::class, 'store'])->name('store');
+    });
+});

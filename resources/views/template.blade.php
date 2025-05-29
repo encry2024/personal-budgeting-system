19d3541ff3bc739e611fdb926246573674e34ad1
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-05-30 03:18:58 +0800

    Fixed indentation.

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,14 +1,14 @@
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Personal Budgeting System</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Personal Budgeting System</title>
 
-    <link rel="stylesheet" href="{{ asset('src/output.css') }}">
-</head>
-<body class="dark:bg-gray-800">
-    <div class="">
-        @yield('content')
-    </div>
-</body>
+        <link rel="stylesheet" href="{{ asset('src/output.css') }}">
+    </head>
+    <body class="dark:bg-gray-800">
+        <div class="">
+            @yield('content')
+        </div>
+    </body>
 </html>

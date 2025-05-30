e5e4b92141ee3c4fffe883d1d818836e036be3b6
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-05-31 00:35:43 +0800

    Added expense index page.

--- resources/views/expense/index.blade.php
+++ resources/views/expense/index.blade.php
@@ -1,3 +1,10 @@
+@extends('template')
+
+@section('content')
+<x-sidebar>
+</x-sidebar>
+
 <div>
     <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
 </div>
+@endsection

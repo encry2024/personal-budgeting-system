dff62bfb3f1cb5a9c5180ac81a42d3751bdf9810
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-05-31 00:35:28 +0800

    Added expense create page.

--- resources/views/expense/create.blade.php
+++ resources/views/expense/create.blade.php
@@ -1,3 +1,7 @@
+@extends('template')
+
+@section('content')
 <div>
-    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
+    
 </div>
+@endsection

22bc53450bf1760436558ef393bbe7b3d3177ed8
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-06-02 01:13:52 +0800

    Reduced the size of the header label.

--- resources/views/expense/index.blade.php
+++ resources/views/expense/index.blade.php
@@ -10,7 +10,7 @@
     <div class="p-5 mr-10 mt-8">
         <div class="flex flex-col dark:bg-white border-md border-solid rounded-md w-full p-3">
             <div class="flex justify-between items-center">
-                <h1 class="text-2xl text-gray-600">
+                <h1 class="text-xl text-gray-600">
                     Expense Category
                 </h1>
 

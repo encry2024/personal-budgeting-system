10158a5d0825623059ffa17398b90127e7cfd397
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-06-02 01:13:24 +0800

    Added UI for create Expense.

--- resources/views/expense/create.blade.php
+++ resources/views/expense/create.blade.php
@@ -1,7 +1,26 @@
 @extends('template')
 
 @section('content')
-<div>
-    
+<div class="grid grid-cols-[256px_1fr] gap-4">
+    <div>
+        <x-sidebar>
+        </x-sidebar>
+    </div>
+
+    <div class="p-5 mr-10 mt-8">
+        <div class="flex flex-col dark:bg-white border-md border-solid rounded-md w-full p-3">
+            <div class="flex justify-between items-center">
+                <h1 class="text-xl text-gray-600">
+                    Create Expense Category
+                </h1>
+            </div>
+        </div>
+
+        <div class="flex flex-col dark:bg-white border-md border-solid rounded-md w-full p-3 mt-5">
+            <div class="">
+                
+            </div>
+        </div>
+    </div>
 </div>
 @endsection

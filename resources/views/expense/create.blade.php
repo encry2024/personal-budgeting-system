52f26a4471dc5e845a0de4cd10b91a0fed9d6a54
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-06-02 11:46:12 +0800

    Added UI for create Expense module.

--- resources/views/expense/create.blade.php
+++ resources/views/expense/create.blade.php
@@ -16,9 +16,32 @@
             </div>
         </div>
 
+        @if(session('message'))
+            <div class="mt-5 p-3 bg-green-500 border-2 border-none rounded-md">
+                <label class="text-white">{{ session('message') }}</label>
+            </div>
+        @endif
+
         <div class="flex flex-col dark:bg-white border-md border-solid rounded-md w-full p-3 mt-5">
-            <div class="">
-                
+            <div class="m-3">
+                <form method="POST" action="{{ route('expense.store') }}">
+                    @csrf
+                    <label class="inline-block mb-5">
+                        <span class="text-gray-700 after:ml-0.5 after:text-red-500 after:content-['*']">Expense Name: </span>
+                        <input type="string" name="name" class="inline-block w-100 rounded-md border
+                            border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm
+                            focus:border-sky-500 focus:outline focus:outline-sky-500 sm:text-sm" />
+                    </label>
+
+                    <div class="block mt-4">
+                        <button type="submit" class="text-[14px] rounded-md bg-green-700 py-2 px-2 text-white w-40 shadow-xl
+                        hover:bg-green-600">Create Expense</button>
+
+                        <a href="{{ route('expense.index') }}" class="text-[14px] border-2 border-none rounded-md
+                        bg-gray-300 py-2 px-2 text-white w-30 shadow-xl float-right dark:bg-gray-500
+                        hover:bg-gray-700 text-center">Cancel</a>
+                    </div>
+                </form>
             </div>
         </div>
     </div>

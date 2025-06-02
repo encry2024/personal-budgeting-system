bdbd9a3cf398d9f1e0d51e53b4ed04cf9e2bb620
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-06-02 11:45:53 +0800

    Added message UI for future purposes.

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -1,3 +1,5 @@
-<div>
-    <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
-</div>
+@if(session('message'))
+    <div class="mt-3 mb-3 p-3 bg-green-500 border-2 border-none rounded-md">
+        <label class="text-white">{{ session('message') }}</label>
+    </div>
+@endif

f74fe684c5160f41e6ce14831824b305d0b8eb46
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-06-02 11:45:09 +0800

    Added return type per function. Added expenses variable that pulls data from the database.

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Expense\StoreExpenseRequest;
 use App\Http\Requests\Expense\UpdateExpenseRequest;
 use App\Models\Expense;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class ExpenseController extends Controller
 {
@@ -16,17 +18,19 @@ class ExpenseController extends Controller
         $this->expense = $expense;
     }
 
-    public function index()
+    public function index(): View
     {
-        return view('expense.index');
+        $expenses = $this->expense->all();
+
+        return view('expense.index')->with('expenses', $expenses);
     }
 
-    public function create()
+    public function create(): View
     {
         return view('expense.create');
     }
 
-    public function store(StoreExpenseRequest $storeExpenseRequest)
+    public function store(StoreExpenseRequest $storeExpenseRequest): RedirectResponse
     {
         $expense = new Expense();
         $expense->name = $storeExpenseRequest->input('name');
@@ -37,14 +41,14 @@ class ExpenseController extends Controller
                 ->with('model', $expense);
     }
 
-    public function update(Expense $expense, UpdateExpenseRequest $updateExpenseRequest)
+    public function update(Expense $expense, UpdateExpenseRequest $updateExpenseRequest): RedirectResponse
     {
         $expenseName = $updateExpenseRequest->input('name');
         $updatedExpense = $expense->update(['name' => $updateExpenseRequest->input('name')]);
 
         if ($updatedExpense) {
             return redirect()->back()->with('model', $expense)
-                ->with("message", "Expense \"" . $expensename . "\" was successfully updated to \"". $updatedExpense->name ."\".");
+                ->with("message", "Expense \"" . $expenseName . "\" was successfully updated to \"". $updatedExpense->name ."\".");
         } else {
             return redirect()->back()
                 ->with('model', $expense)

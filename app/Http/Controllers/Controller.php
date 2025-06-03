54ff4ca92a82fd7dd67ed7e79e294a7b7292e975
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-06-03 23:12:51 +0800

    Added modelExists function to check whether the name that is entered by the users either exists or soft deleted.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Model;
+
 abstract class Controller
 {
-    //
+    protected function modelExists(Model $model, string $column, string $value): bool
+    {
+        $obj = $model->withTrashed()->where($column, $value)->first();
+
+        if ($obj) {
+            return true;
+        }
+
+        return false;
+    }
 }

68a112d37e1948b3fca6e64fe62d41e05e7a363d
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2025-06-03 23:14:09 +0800

    Fixed destroy function and added dynamic responses. Added modelExists checker.

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -6,6 +6,8 @@ use App\Http\Requests\Expense\StoreExpenseRequest;
 use App\Http\Requests\Expense\UpdateExpenseRequest;
 use App\Models\Expense;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 
 class ExpenseController extends Controller
@@ -37,39 +39,79 @@ class ExpenseController extends Controller
 
     public function store(StoreExpenseRequest $storeExpenseRequest): RedirectResponse
     {
+        if ($this->modelExists($this->expense, 'name', $storeExpenseRequest->input('name')) instanceof Expense) {
+            return redirect()->back()
+                ->with('message', 'Cannot update expense name to this name as it already exists .')
+                ->with('messageColor', 'bg-red-500');
+        }
+
         $expense = new Expense();
         $expense->name = $storeExpenseRequest->input('name');
-        $expense->save();
 
-        return redirect()->back()
+        if ($expense->save()) {
+            return redirect()->back()
                 ->with('message', 'Expense "' . $expense->name . '" was successfully created.')
-                ->with('model', $expense);
+                ->with('model', $expense)
+                ->with('messageColor', 'bg-green-500');
+        }
+
+        return redirect()->back()->with('message', 'Something went wrong, please try again later.');
     }
 
+    /**
+     * @TODO check if the user input is the same as the current name of the expense
+     * if yes: return different message
+     *
+     * @param Expense $expense
+     * @param UpdateExpenseRequest $updateExpenseRequest
+     * @return RedirectResponse
+     */
     public function update(Expense $expense, UpdateExpenseRequest $updateExpenseRequest): RedirectResponse
     {
         $expenseName = $expense->name;
+
+        if ($this->modelExists($this->expense, 'name', $updateExpenseRequest->input('name'))) {
+            return redirect()->back()
+                ->with('message', 'Expense already exists!')
+                ->with('messageColor', 'bg-red-500');
+        }
+
         $updatedExpense = $expense->update(['name' => $updateExpenseRequest->input('name')]);
 
         if ($updatedExpense) {
-            return redirect()->back()->with('model', $expense)
-                ->with("message", "Expense \"" . $expenseName . "\" was successfully updated to \"". $expense->name ."\".");
-        } else {
             return redirect()->back()
                 ->with('model', $expense)
-                ->with('message', 'An error occurred while updating Expense.');
+                ->with('messageColor', 'bg-green-500')
+                ->with("message", "Expense \"" . $expenseName . "\" was successfully updated to \"". $expense->name ."\".");
         }
+
+        return redirect()->back()
+            ->with('model', $expense)
+            ->with('messageColor', 'bg-red-500')
+            ->with('message', 'An error occurred while updating "'. $expenseName .'".');
     }
 
-    public function destroy(Expense $expense): RedirectResponse
+    public function destroy(Expense $expense): JsonResponse
     {
         $expenseName = $expense->name;
 
         if ($expense->delete()) {
-            return redirect()->back()->with('model', $expense)
-                ->with('message', 'Expense "' . $expenseName . '" was successfully deleted.');
+            return response()
+                ->json([
+                    'message' => 'Expense "' . $expenseName . '" was successfully deleted.',
+                    'model' => $expense,
+                    'icon' =>  'success',
+                    'color' => '#00a63e',
+                    'status' => 200
+                ]);
         }
 
-        return redirect()->back()->with('message', 'An error occurred while deleting Expense: "'. $expenseName .'".');
+        return response()->json([
+            'message' => 'An error occurred while deleting Expense.',
+            'model' => $expense,
+            'icon' =>  'error',
+            'color' => '#ea5b5b',
+            'status' => 500
+        ], 500);
     }
 }

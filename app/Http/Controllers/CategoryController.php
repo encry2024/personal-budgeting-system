a91b66853b7c892829ebfec31126eb334504bce3
Christan Jake Gatchalian <christan.gatchalian0820@gmail.com>
2025-08-13 00:33:20 +0800

    Fixed incorrect namespace for category request. Added main page, edit page, store, and update functionality.

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\Category\StoreCategoryRequest;
+use App\Http\Requests\Category\UpdateCategoryRequest;
+use App\Http\Requests\Category\DestroyCategoryRequest;
+use App\Http\Requests\Category\ForceDestroyCategoryRequest;
+use App\Models\Attribute;
+use App\Models\Category;
+use Auth;
+
+class CategoryController extends Controller
+{
+    protected Category $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
+    public function index()
+    {
+        $expenseCategories = Category::withTrashed()->get();
+
+        return view('category.index')->with('expenseCategories', $expenseCategories);
+    }
+
+    public function create()
+    {
+        return view('category.create');
+    }
+
+    public function store(StoreCategoryRequest $request)
+    {
+        $category = new category();
+        $category->name = $request->name;
+        $category->user_id = Auth::user()->id;
+
+        if ($category->save()) {
+            return redirect()->back();
+        }
+    }
+
+    public function edit(Category $category)
+    {
+        return view('category.edit')->withCategory($category);
+    }
+
+    public function update(Category $category, UpdateCategoryRequest $request)
+    {
+        $categoryName = $category->name;
+
+        if ($this->modelExists($this->category, 'name', $request->name)) {
+            return redirect()->back()
+                ->with('message', 'Cannot update category name to this name as it already exists or was only temporarily deleted.')
+                ->with('messageColor', config('response.color.error'));
+        }
+
+        $updatedCategory = $category->update(['name' => $request->name]);
+
+        if ($updatedCategory) {
+            return redirect()->back()
+                ->with('model', $category)
+                ->with('messageColor', config('response.color.success'))
+                ->with("message", 'Expense category "' . $categoryName . '" was successfully updated to "'. $category->name .'".');
+        }
+
+        return redirect()->back()
+            ->with('model', $category)
+            ->with('message', 'An error occurred while updating "'. $categoryName .'".')
+            ->with('messageColor', config('response.color.error'));
+    }
+
+    public function destroy(DestroyExpenseRequest $request)
+    {
+        // code...
+    }
+
+    public function restore()
+    {
+        // code...
+    }
+
+    public function forceDelete()
+    {
+        // code...
+    }
+}

699a37eb9b363e4af735c72099b3db1081d8000d
Christan Jake Gatchalian <christan.gatchalian0820@gmail.com>
2025-08-18 04:03:23 +0800

    Added fillables and Expense relationship.

--- app/Models/ExpenseAttributeValue.php
+++ app/Models/ExpenseAttributeValue.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Expense;
 
 class ExpenseAttributeValue extends Model
 {
     //
+
+    protected $table = 'expense_attribute_values';
+
+    protected $fillable = ['expense_id', 'attribute_id', 'value'];
+
+    public function expense()
+    {
+        return $this->balongsTo(Expense::class);
+    }
 }

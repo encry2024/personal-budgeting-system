2d3b1f3c4af775a33e3093b00d2cc6eb4adb9627
Christan Jake Gatchalian <christan.gatchalian0820@gmail.com>
2025-08-25 12:04:44 +0800

    Created: Create view for attribute.

---
+++ resources/views/attribute/create.blade.php
@@ -0,0 +1,72 @@
+@extends('template')
+
+@section('content')
+<div class="grid grid-cols-[256px_1fr] gap-4">
+    <div>
+        <x-sidebar>
+        </x-sidebar>
+    </div>
+
+    <div class="p-5 mr-10 mt-8">
+        <div class="flex flex-col dark:bg-white border-md border-solid rounded-md w-full p-3">
+            <div class="flex justify-between items-center">
+                <h1 class="text-xl text-gray-600">
+                    Create Attribute
+                </h1>
+            </div>
+        </div>
+
+        @if(request()->session()->has('message'))
+            <div class="mt-5 p-3 {{ session('messageColor') }} border-2 border-none rounded-md">
+                <label class="text-white">{{ session('message') }}</label>
+            </div>
+        @endif
+
+        <div class="flex flex-col dark:bg-white border-md border-solid rounded-md w-full p-3 mt-5">
+            <div class="m-3">
+                <form method="POST" action="{{ route('attribute.store') }}" class="space-y-4">
+                    @csrf
+                    
+                    <!-- Row 1 -->
+                    <div class="flex items-center gap-4">
+                        <label for="name" class="w-40 text-gray-700 after:ml-0.5 after:text-red-500 after:content-['*']">
+                            Attribute Name:
+                        </label>
+                        <input type="text" id="name" name="name" 
+                            class="w-[40%] rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm
+                                   focus:border-sky-500 focus:outline focus:outline-sky-500 sm:text-sm" />
+                    </div>
+
+                    <!-- Row 2 -->
+                    <div class="flex items-center gap-4">
+                        <label for="type" class="w-40 text-gray-700 after:ml-0.5 after:text-red-500 after:content-['*']">
+                            Attribute Type:
+                        </label>
+                        <select id="type" name="type" class="w-[40%] rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm
+                        focus:border-sky-500 focus:outline focus:outline-sky-500 sm:text-sm" />
+                            <option value="0">Text</option>
+                            <option value="1">Numbers only</option>
+                            <option value="2">Date only</option>
+                       </select>
+                    </div>
+
+                    <!-- Buttons -->
+                    <div class="block pt-4 mt-4">
+                        <button type="submit" 
+                            class="text-[14px] rounded-md bg-green-700 py-2 px-4 text-white shadow-xl
+                                   hover:bg-green-600">
+                            Create
+                        </button>
+
+                        <a href="{{ route('attribute.index') }}" 
+                            class="text-[14px] rounded-md bg-gray-500 py-2 px-4 text-white shadow-xl
+                                   hover:bg-gray-700 text-center float-right">
+                            Cancel
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

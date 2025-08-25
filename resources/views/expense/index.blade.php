4fa33b2dce30df2f941785443554f7e78ef24d1a
Christan Jake Gatchalian <christan.gatchalian0820@gmail.com>
2025-08-25 11:55:19 +0800

    Added: Permanently delete link and function.

--- resources/views/expense/index.blade.php
+++ resources/views/expense/index.blade.php
@@ -22,7 +22,7 @@
                               d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                     </svg>
 
-                    New Expense
+                    Create Expense Category
                 </a>
             </div>
         </div>
@@ -94,6 +94,20 @@
                             <tr class="group">
                                 <td class="border border-gray-300 px-4 py-2 group-hover:bg-gray-50">
                                     {{ $temporarilyDeletedExpense->name }}
+
+                                    <button class="permanently-delete-expense flex items-center gap-2 text-[14px] rounded-md bg-red-600 py-2 px-2
+                                           text-white w-8 shadow-xl float-right dark:bg-red-700 hover:bg-red-800
+                                           text-center ml-1"
+                                            data-expense-id="{{ $temporarilyDeletedExpense->id }}"
+                                            data-expense-name="{{ $temporarilyDeletedExpense->name }}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                            class="w-4 h-4">
+                                            <path
+                                                fill-rule="evenodd"
+                                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" clip-rule="evenodd" />
+                                        </svg>
+
+                                    </button>
 
                                     <button
                                         class="delete-expense flex items-center gap-2 text-[14px] rounded-md bg-red-600 py-2 px-2
@@ -217,6 +231,56 @@
                             if (xhr.status === 200) {
                                 Swal.fire({
                                     title: 'Expense restored successfully',
+                                    icon: 'success',
+                                    html: `${data.message}`,
+                                    confirmButtonText: 'Ok',
+                                }).then((result) => {
+                                    if (result.isConfirmed) {
+                                        location.reload();
+                                    }
+                                });
+                            } else {
+                                Swal.fire({
+                                    title: 'Error',
+                                    icon: 'error',
+                                    html: `${data.message}`,
+                                    confirmButtonText: 'Ok',
+                                });
+                            }
+                        }
+                    });
+                }
+            });
+        });
+
+        $('.permanently-delete-expense').on('click', function (e) {
+            let expenseName = $(this).attr('data-expense-name');
+            let expenseId = $(this).attr('data-expense-id');
+
+            let urlString = "{{ route('expense.force_delete', ":expenseId") }}";
+            let permanentDeleteRoute = urlString.replace(':expenseId', expenseId);
+
+            Swal.fire({
+                title: 'Permanently Delete Expense',
+                html: `Are you sure you want to permanently delete ${expenseName}? Expense`,
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Permanently Delete',
+                confirmButtonColor: '#ea5b5b',
+                cancelButtonText: 'Cancel',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    $.ajax({
+                        type: 'POST',
+                        url: permanentDeleteRoute,
+                        dataType: 'json',
+                        data: {
+                            _token: "{{ csrf_token() }}"
+                        },
+                        success: function (data, textStatus, xhr) {
+                            if (xhr.status === 200) {
+                                Swal.fire({
+                                    title: 'Delete Expense Permanently',
                                     icon: 'success',
                                     html: `${data.message}`,
                                     confirmButtonText: 'Ok',

c18652bbc919622983a2824f5d3c72d8cc953be7
Christan Jake Gatchalian <christan.gatchalian0820@gmail.com>
2025-10-07 23:32:19 +0800

    Fix: Changed to appropriate route. Changed icon.

--- resources/views/expense/index.blade.php
+++ resources/views/expense/index.blade.php
@@ -60,7 +60,7 @@
                                         </svg>
                                     </button>
 
-                                    <a href="{{ route('expense.edit', $expense->id) }}"
+                                    <a href="{{ route('expense.show', $expense->id) }}"
                                        class="flex items-center gap-2 text-[14px] rounded-md bg-green-500 py-2 px-2
                                        text-white w-8 shadow-xl float-right dark:bg-green-600 hover:bg-green-700
                                        text-center">
@@ -68,10 +68,9 @@
                                              stroke-width="1.5" stroke="currentColor" class="w-4 h-4"
                                              aria-hidden="true">
                                             <path stroke-linecap="round" stroke-linejoin="round"
-                                                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832
-                                                  19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1
-                                                  1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                                                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                                         </svg>
+
                                     </a>
                                 </td>
                             </tr>

1d0c2d64a45ff0d50ada7a3f61988863618ecb52
Christan Jake Gatchalian <christan.gatchalian0820@gmail.com>
2025-10-09 06:00:33 +0800

    Feat: Added Category model and belongsTo relationship.

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\ExpenseAttributeValue;
+use App\Models\Category;
 
 class Expense extends Model
 {
@@ -16,5 +17,10 @@ class Expense extends Model
     public function attributes()
     {
         return $this->hasMany(ExpenseAttributeValue, 'expense_id', 'id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 }

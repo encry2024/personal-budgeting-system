9b4bea3d4457c339c79f68411f1e8980bdf6c619
Christan Jake Gatchalian <christan.gatchalian0820@gmail.com>
2025-10-10 18:31:52 +0800

    Feat (Expense): Added factory.

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\ExpenseAttributeValue;
 use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Expense extends Model
 {
     //
-    use SoftDeletes;
+    use SoftDeletes, HasFactory;
 
     protected $fillable = ['user_id', 'name', 'category_id'];
 
